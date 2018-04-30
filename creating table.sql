bf9b07ead99001c8c468d8895e2fb51f42a653d0
pramithaprasad <37370142+pramithaprasad@users.noreply.github.com>
2018-04-30 12:10:52 +0530

    Create creating table.sql

---
+++ creating table.sql
@@ -0,0 +1,3 @@
+create table students(sl_no varchar(10),name varchar(20),roll_no varchar(20),department varchar(10),college_name varchar(10));
+insert into students values('1','pramitha','15bec041','ece','kct');
+select * from students;
